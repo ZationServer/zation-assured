2d805c8d45205bab95fef57eb72ac138b97a28b2
Luca Scaringella <lucalstyle@gmail.com>
2021-11-04 18:17:58 +0100

    Update copyright notice.

--- src/lib/helper/builder/controller/standardRequestBuilder.ts
+++ src/lib/helper/builder/controller/standardRequestBuilder.ts
@@ -1,7 +1,7 @@
 /*
-Author: Luca Scaringella
+Author: Ing. Luca Gian Scaringella
 GitHub: LucaCode
-©Copyright by Luca Scaringella
+Copyright(c) Ing. Luca Gian Scaringella
  */
 
 import {StandardRequestBuilder as NativeStandardRequestBuilder, Client} from "zation-client";
