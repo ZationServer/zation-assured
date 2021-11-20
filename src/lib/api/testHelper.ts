e37a01f50a0045eb3540dcbccc828e8d0f197784
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2021-11-20 13:27:38 +0100

    Remove unnecessary test helper functions.

--- src/lib/api/testHelper.ts
+++ src/lib/api/testHelper.ts
@@ -4,70 +4,9 @@ GitHub: LucaCode
 Copyright(c) Ing. Luca Gian Scaringella
  */
 
-import {Logger} from "../helper/console/logger";
-
 const assert = require('assert');
 
-type Func = () => Promise<void> | void;
-
-export const beforeTest = (beforeTest: Func, timeout: number = 10000) => {
-    firstInit();
-    before(async function () {
-        this.timeout(timeout);
-        await beforeTest();
-    });
-};
-
-export const afterTest = (afterTest: Func, timeout: number = 10000) => {
-    after(async function () {
-        this.timeout(timeout);
-        await afterTest();
-    });
-};
-
-export const beforeEachTest = (beforeEachTest: Func, timeout: number = 10000) => {
-    firstInit();
-    beforeEach(async function () {
-        this.timeout(timeout);
-        await beforeEachTest;
-    });
-};
-
-export const afterEachTest = (afterEachTest: Func, timeout: number = 10000) => {
-    afterEach(async function () {
-        this.timeout(timeout);
-        await afterEachTest;
-    });
-};
-
-export const describeTest = (title: string, area: Func, timeout: number = 10000) => {
-    firstInit();
-    describe(title, async function () {
-        this.timeout(timeout);
-        await area();
-    });
-};
-
-export const itTest = (title: string, area: Func, timeout: number = 10000) => {
-    firstInit();
-    it(title, async function () {
-        this.timeout(timeout);
-        await area();
-    });
-};
-
-export const failTest = (msg: string, actual ?: any, expected ?: any) => {
-    if (actual !== undefined || expected !== undefined) {
-        assert.fail(actual, expected, msg);
-    } else {
-        assert.fail(msg);
-    }
-};
-
-let firstCall = true;
-const firstInit = () => {
-    if (firstCall) {
-        firstCall = false;
-        Logger.logBusy('Run Tests');
-    }
+export const failTest = function(msg: string, actual ?: any, expected ?: any) {
+    if (arguments.length > 1) assert.fail(actual, expected, msg);
+    else assert.fail(msg);
 };
