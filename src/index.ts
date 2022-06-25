8fd71cee2481166da940a81a77434f7e717cd438
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-06-25 11:56:20 +0200

    Update exports.

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,13 @@ GitHub: LucaCode
 Copyright(c) Ing. Luca Gian Scaringella
  */
 
-//Api Classes
-import {ClientOptions, Client, create} from "zation-client";
 import {when} from "./lib/api/when";
-
-import {
-    failTest as fail
-}
-    from "./lib/api/testHelper";
+import {failTest as fail} from "./lib/api/testHelper";
 import {assert} from "./lib/api/assert";
 
 export * from 'zation-client';
 export {
-    create as createClient,
-    ClientOptions,
     when,
     assert,
-    fail,
-    Client
+    fail
 };
